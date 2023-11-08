194d280882e0db6464a6e106604d6bdd862e5b0f
DerekWan <ug61837361636@gmail.com>
2023-11-08 16:10:13 +0800

    add project.js

---
+++ server/routes/route/project.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  console.log(req.method, req.url);
+  next();
+});
+let arr = [];
+router.get("/getproject", (req, res) => {
+  try {
+    arr = [];
+    const username = req.query.username;
+    const user_path = path.join(__dirname, "../../uploads", username);
+    console.log(username, user_path);
+    if (fs.existsSync(user_path)) {
+      console.log("folder exists");
+      fs.readdirSync(user_path).forEach((folder) => {
+        console.log(folder);
+        arr.push(folder);
+      });
+      console.log(arr);
+    } else {
+      console.log("no such folder");
+      res.status(500).json("no such folder");
+    }
+    res.status(200).json(arr);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+router.post("/addproject", (req, res) => {
+  const username = req.query.username;
+  const projectname = req.body.projectName;
+  console.log(projectname);
+  const previousDir = path.join(__dirname, "..");
+  const dir = path.join(previousDir, "../uploads", username, projectname);
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+    res.send("新增成功!");
+  } else {
+    res.send("專案已存在!");
+  }
+});
+
+router.post("/deleteproject", (req, res) => {
+  const username = req.query.username;
+  console.log(username);
+  const projectname = req.body.projectName;
+  console.log(projectname);
+  const previousDir = path.join(__dirname, "..");
+  const dir = path.join(previousDir, "../uploads", username, projectname);
+
+  try {
+    // Use recursive option to remove the directory and its contents
+    fs.rmdirSync(dir, { recursive: true });
+    res.send("專案已刪除!"); // Project has been deleted
+  } catch (err) {
+    console.error("Error deleting directory:", err);
+    res.status(500).send("刪除專案時發生錯誤"); // Error occurred while deleting
+  }
+});
+
+module.exports = { router };
